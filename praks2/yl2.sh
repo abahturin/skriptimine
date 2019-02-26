0645ade38e1a74f3d6ee1941551fa9d12e1c4575
root <root@script.script>
2019-02-26 13:31:43 +0200

    yl2

---
+++ praks2/yl2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#see skript sisaldab minu andmeid, mõteid jne
+echo "--------------------"
+echo "Aleksander Bahturin"
+
+echo "IS118"
+sleep  1
+echo "Meeldib"
+sleep 0.5
+echo "no comments"
+sleep 0.5
+echo "jne"
+echo "--------------------"
