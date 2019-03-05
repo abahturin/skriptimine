f845e703f06a0218168fd5936efcc955e473fc64
Aleksander Bahturin <aleksander.bahturin@khk.ee>
2019-03-05 12:52:16 +0200

    M_Karutin_commit

---
+++ praks3/yl1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+now=$(date +"%T")		#
+date=$(date +"%A, %B, %C")	# määravad parameetreid
+calender=$ cal			#
+echo				#reavahe mugavaks kasutamiseks
+echo "Today is: " $date		#prindib kuupäev
+echo
+echo "Current time: " $now	#prindib kella
+echo
+echo $ cal  			#prindib kalendri
+
